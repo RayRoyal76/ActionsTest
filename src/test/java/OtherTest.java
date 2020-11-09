5fcc310c0a1bc1aaec913619cf6731c7cd8cab34
RayRoyal1876 <tenthwonderind@icloud.com>
2020-11-09 10:48:22 -0500

    next

---
+++ src/test/java/OtherTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class OtherTest {
+    @Test
+    public void test(){
+        fail("not yet implemented");
+    }
+}
